7d37b03da83b92ddbf474482604f36a516ab52ea
Vishwajeet Pandey <32550846+Vishwa2611@users.noreply.github.com>
2021-07-31 02:58:42 +0530

    Create StarPattern1.java
    StarPattern1 - " * ** *** *** ** *  " - program added

---
+++ StarPattern1.java
@@ -0,0 +1,49 @@
+//Java program to print    ` * ** *** *** ** * `  pattern based
+
+public class StarPattern1 {
+
+    public static void main(String[] args) {
+
+        int n = 30;
+        int i= 0;
+        int mid = n / 2;
+
+        for(i = 1 ; i <= n ; i++){
+
+            boolean midCheck =  i <= mid ;
+
+            int j;
+
+            boolean cond = true;
+
+            if(midCheck){
+                j= 0;
+                cond = j < i;
+            }else{
+                j = n - i ;
+            }
+
+            while(cond) {
+
+                System.out.print("*");
+
+                if(midCheck){
+
+                    j++;
+
+                    cond = j < i;
+
+                }else{
+
+                    cond = j >= 1;
+
+                    j--;
+                }
+            }
+
+
+            System.out.print(" ");
+        }
+
+    }
+}
